18ca1659f6f44e430256039cedf687c3c86e6c37
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-25 16:00:13 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,16 @@
-HexoPlusPlus@0.0.1
+const hpp_now='HexoPlusPlus@0.0.1'
+if(hpp_now == hpp_ver){
+  swal({
+title: "成功",
+    text: "无需更新，当前已是最新版本",
+    icon: "success",
+});
+}else{
+   swal({
+title: "成功",
+    text: "需要更新，点击确定获取最新代码",
+    icon: "warning"})
+	.then(() => {
+location.href="https://cdn.jsdelivr.net/gh/HexoPlusPlus/HexoPlusPlus@latest/index.js"
+})
+}
