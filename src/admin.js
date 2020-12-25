ca47d7ef51b1b078eb98ce3e5bf070cdbb43a922
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-25 15:58:58 +0800

    Update admin.js

--- src/admin.js
+++ src/admin.js
@@ -192,39 +192,3 @@ $(document).ready(function() {
 	getCDNinfo();
     //页面加载完毕就获取CDN信息
 });
-function hpp_update(){
-		input.disabled=true
-	var ajax = ajaxObject();
-    ajax.open( "get" , '/hpp/admin/api/update' , true );
-    ajax.setRequestHeader( "Content-Type" , "test/plain" );
-    ajax.onreadystatechange = function () {
-	    const hpp_now = ajax.responseText;
-        if( ajax.readyState == 4 ) {
-            if( hpp_now == hpp_ver ) {
-                swal({
-title: "成功",
-    text: "需要更新，点击确定获取最新代码",
-    icon: "warning"})
-	.then(() => {
-location.href="https://cdn.jsdelivr.net/gh/HexoPlusPlus/HexoPlusPlus@latest/index.js"
-})
-
-
-            }else{
-			swal({
-title: "成功",
-    text: "无需更新，当前已是最新版本",
-    icon: "success",
-
-
-});
-				
-            }
-            
-        }
-	    else {
-                sweetAlert("糟糕", "获取更新失败", "error");
-            }
-    }
-    ajax.send();
-	};
