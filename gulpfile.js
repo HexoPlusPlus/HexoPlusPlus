e1d4e252a58916b44aebb9cbb39cc11a4b0defad
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-26 18:29:56 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,39 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+const { task, src, dest, series } = require('gulp')
+const minifycss = require('gulp-minify-css')
+const terser = require('gulp-terser')
 
-gulp.task("uglify",function(){
-    gulp
-        .src('js/*.js')       
-        .pipe(uglify())          
-        .pipe(gulp.dest('dist')) 
-})
+const css = function () {
+    return src('src/*.css')
+        .pipe(minifycss())
+        .pipe(dest('dist'))
+        .pipe(minifycss())
+}
+css.displayName = 'minifycss'
+task(css);
+
+const js = function () {
+    return src(['src/**/*.js'])
+        .pipe(terser())
+        .pipe(dest('dist'))
+        .pipe(terser())
+}
+js.displayName = 'minifyjs'
+task(js)
+
+const copyIndex = function () {
+    return src('index.js')
+        .pipe(dest('dist'))
+}
+copyIndex.displayName = 'copyindexjs'
+task(copyIndex)
+
+const copyUpdate = function () {
+    return src('update.js')
+        .pipe(dest('dist'))
+}
+copyUpdate.displayName = 'copyupdatejs'
+task(copyUpdate)
+
+task('copy', series(['copyindexjs','copyupdatejs']))
+
+task('default', series(['minifycss','minifyjs', 'copy']))
