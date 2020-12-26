61770fce856b29637a3f2521d89afdd4d8118c59
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-12-26 17:14:16 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 gulp.task('script', done => {
-gulp.src('js/*.js')
+gulp.src('src/*.js')
 .pipe(uglify())
 .pipe(gulp.dest('dist/js'))
 done()
