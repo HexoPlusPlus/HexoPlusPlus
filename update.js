5406d67d7cc0340b691a1bb400851431a2b80fad
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2021-01-30 16:47:09 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,15 +1,57 @@
-if(hpp_ver == 'HexoPlusPlus@0.1.1'){
+function hpp_update(){
+var ajax = ajaxObject();
+    ajax.open( "get" , '/hpp/admin/api/update' , true );
+    ajax.setRequestHeader( "Content-Type" , "text/plain" );
+    ajax.onreadystatechange = function () {
+        if( ajax.readyState == 4 ) {
+            if( ajax.responseText == "true" ) {
+                swal("更新完毕！", {
+  icon: "success",
+  buttons: {
+    yes: "是"
+  },
+})
+.then((value) => {
+  switch (value) {
+    default:
+	  window.location.reload();
+  }
+});
+            }
+            else {
+                sweetAlert("糟糕", "更新失败!", "error");
+            }
+        }
+    }
+    ajax.send();
+}
+
+if(hpp_ver == 'HexoPlusPlus@0.1.3'){
   swal({
 title: "成功",
     text: "无需更新，当前已是最新版本",
     icon: "success",
 });
 }else{
-   swal({
-title: "成功",
-    text: "紧急安全性更新，请及时更新",
-    icon: "warning"})
-	.then(() => {
-location.href="https://cdn.jsdelivr.net/gh/HexoPlusPlus/HexoPlusPlus@main/index.js"
+swal("存在更新，是否更新？", {
+  icon: "warning",
+  buttons: {
+    cancel: "否",
+    update: "是"
+  },
 })
+.then((value) => {
+  switch (value) {
+ 
+    case "update":
+      hpp_update();
+	  break;
+ 
+    default:
+      swal("更新已取消",{icon: "warning"});
+	  break;
+  }
+});
 }
+
+
