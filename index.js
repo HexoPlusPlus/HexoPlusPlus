53746531f860a7c8d00ce895bdc7f533755cf8cc
ChenYFan <ChenYFan>
2021-02-07 12:16:22 +0800

    静态资源更新

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const hpp_CDNver = "3824155"
-const hpp_ver = "HexoPlusPlus@1.0.9_β_11"
+const hpp_CDNver = "e1f87e4"
+const hpp_ver = "HexoPlusPlus@1.0.9_β_12"
 const dev_mode_branch = "dist"
 let hpp_logstatus = 0
 
