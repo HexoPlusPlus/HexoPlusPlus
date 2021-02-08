5fcfe3aac4c7f8896ab9ce757e7e5f226fd92a1b
ChenYFan <ChenYFan>
2021-02-08 10:09:09 +0800

    1.1.0

--- src/talk.js
+++ src/talk.js
@@ -494,4 +494,16 @@ new hpp_md_editor({
 	owo: hpp_OwO,
 	backuptime: 60000
 })
-
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+    return highlight.highlightAuto(code).value;
+  }
+});
