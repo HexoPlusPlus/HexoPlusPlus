cee90baf017b78d248f3a3c8e8595bad27aec6ca
ChenYFan <ChenYFan>
2021-02-09 11:36:31 +0800

    1.1.1βLatest

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const hpp_CDNver = "ff6a6a5"
-const hpp_ver = "HexoPlusPlus@1.1.1_β_10"
+const hpp_CDNver = "b56c61b"
+const hpp_ver = "HexoPlusPlus@1.1.1_β_11"
 const dev_mode_branch = "dist"
 let hpp_logstatus = 0
 
