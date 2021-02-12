c96fe3d1ffb1a273fd1dd234aefb4792a3b54f09
ChenYFan <ChenYFan>
2021-02-12 22:24:52 +0800

    1.1.3β

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const md5=require ('md5')
+//const md5=require ('md5')
 
 //开发者请将上述依赖注释去除
 
-const hpp_CDNver = "9e14d26"
-const hpp_ver = "HexoPlusPlus@1.1.3_β_6"
+const hpp_CDNver = "afc453e"
+const hpp_ver = "HexoPlusPlus@1.1.3_β_7"
 const dev_mode_branch = "dist"
 let hpp_logstatus = 0
 
