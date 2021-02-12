ac08168958972b658da49846528fd5a33a621224
ChenYFan <ChenYFan>
2021-02-12 15:03:07 +0800

    1.1.3小白鼠

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const hpp_CDNver = "12a4568"
-const hpp_ver = "HexoPlusPlus@1.1.3_β_3"
+const hpp_CDNver = "a02d199"
+const hpp_ver = "HexoPlusPlus@1.1.3_β_4"
 const dev_mode_branch = "dist"
 let hpp_logstatus = 0
 
