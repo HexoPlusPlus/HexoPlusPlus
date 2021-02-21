c4f323c17a78c3acd6846e274e31ceca08623f18
ChenYFan <chen2778754364@foxmail.com>
2021-02-21 10:25:54 +0800

    1.2.0Upstatic

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 //开发者请将上述依赖注释去除
 
-const hpp_CDNver = "d0f1fea"
-const hpp_ver = "HexoPlusPlus@1.2.0_β_14"
+const hpp_CDNver = "c70302e"
+const hpp_ver = "HexoPlusPlus@1.2.0_β_15"
 const dev_mode_branch = "dist"
 let hpp_logstatus = 0
 
